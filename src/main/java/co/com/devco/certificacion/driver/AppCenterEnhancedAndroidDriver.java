d17883cf0e8b586fb89dbfa4fa85d4bcfe7afc2c
Jennifer Perez Bedoya <jperezb@devcognitio.com>
2018-10-05 12:30:29 -0500

    Se agrega diver para appcenter

--- src/main/java/co/com/devco/certificacion/driver/AppCenterEnhancedAndroidDriver.java
+++ src/main/java/co/com/devco/certificacion/driver/AppCenterEnhancedAndroidDriver.java
@@ -2,10 +2,10 @@ package co.com.devco.certificacion.driver;
 
 import co.com.devco.certificacion.driver.exceptions.FailedDriverCreationException;
 import co.com.devco.certificacion.driver.exceptions.LoadDriverCapabilitiesException;
+import com.microsoft.appcenter.appium.EnhancedAndroidDriver;
+import com.microsoft.appcenter.appium.Factory;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
-import net.serenitybdd.core.SerenitySystemProperties;
-import net.thucydides.core.ThucydidesSystemProperty;
 import net.thucydides.core.guice.Injectors;
 import net.thucydides.core.util.EnvironmentVariables;
 import org.openqa.selenium.WebDriver;
@@ -15,17 +15,16 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static co.com.devco.certificacion.driver.exceptions.FailedDriverCreationException.FAILED_DRIVER_CREATION;
-import static net.thucydides.core.ThucydidesSystemProperty.THUCYDIDES_TIMEOUT;
 
-public class OwnAppiumDriver extends Driver {
+public class AppCenterEnhancedAndroidDriver extends Driver {
 
     private final DesiredCapabilities capabilities;
-    private AppiumDriver<MobileElement> driver;
+    private EnhancedAndroidDriver<MobileElement> driver;
 
     private static final String PLATFORM_NAME_CAP = "appium.platformName";
     private static final String APPIUM_URL_CAP = "hub";
 
-    private OwnAppiumDriver() throws LoadDriverCapabilitiesException {
+    private AppCenterEnhancedAndroidDriver() throws LoadDriverCapabilitiesException {
         this.capabilities = super.loadCapabilities(Platform.MOBILE);
         EnvironmentVariables environmentVariables = Injectors.getInjector().getInstance(EnvironmentVariables.class);
         environmentVariables.setProperty(PLATFORM_NAME_CAP, this.capabilities.getPlatform().name());
@@ -35,7 +34,7 @@ public class OwnAppiumDriver extends Driver {
     public AppiumDriver init() throws MalformedURLException {
         if (this.driver == null) {
             URL url = new URL(capabilities.getCapability(APPIUM_URL_CAP).toString());
-            this.driver = new AppiumDriver<>(url, capabilities);
+            this.driver = Factory.createAndroidDriver(url, capabilities);
         }
         return this.driver;
     }
@@ -43,7 +42,7 @@ public class OwnAppiumDriver extends Driver {
     public static WebDriver getDriver() throws FailedDriverCreationException {
         Driver customDriver;
         try {
-            customDriver = new OwnAppiumDriver();
+            customDriver = new AppCenterEnhancedAndroidDriver();
             return (WebDriver) customDriver.init();
         } catch (LoadDriverCapabilitiesException | MalformedURLException e) {
             throw new FailedDriverCreationException(FAILED_DRIVER_CREATION + e.getMessage(), e.getCause());
